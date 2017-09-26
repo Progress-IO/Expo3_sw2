05b5f08710c0125cea98d78e0937bb9181961ccf
Fabian Mendez <ofmendez@unal.edu.co>
2017-09-26 04:36:40 +0000

    Modificado el routes para dejar la opcion ver perfil por defecto en root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get 'profile/edit'
 
+  root to: 'profile#view'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
