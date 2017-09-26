38d8922b1dc5185cb20311d5108e245e9b581f71
Fabian Mendez <ofmendez@unal.edu.co>
2017-09-26 18:55:17 +0000

    Agregada imagen y boton a la vista del perfil

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'profile/view'
 
-  get 'profile/edit'
+  get 'profile/edit', to: 'profile#edit', as: 'profile'
+
 
   root to: 'profile#view'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
