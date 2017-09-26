1af767eefbce7e1761f48bc6a24e88d5b5aa9105
Fabian Mendez <ofmendez@unal.edu.co>
2017-09-26 04:33:55 +0000

    Creado controlador de perfil mediante: rails generate controller Profile view edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'profile/view'
+
+  get 'profile/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
